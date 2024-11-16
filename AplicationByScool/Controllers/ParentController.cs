784cdc045c142f68502837c8dc7f30cc322cd5c2
Hristina Zheleva <donat@abv.bg>
2024-11-16 22:48:08 +0200

    Create Views Index an Create Create ParentController

---
+++ AplicationByScool/Controllers/ParentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolDairy.Data;
+using SchoolDairy.Data.Models;
+
+namespace SchoolDairy.Controllers
+{
+    public class ParentController : Controller
+    {
+        private readonly SchoolDairyDbContext schoolDairyDbContext;
+
+        public ParentController(SchoolDairyDbContext schoolDairyDbContext)
+        {
+            this.schoolDairyDbContext = schoolDairyDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Parent> parents = this.schoolDairyDbContext
+                 .Parents
+                 .ToList();
+
+            return View(parents);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Parent parent)
+        {
+            this.schoolDairyDbContext.Parents.Add(parent);
+            this.schoolDairyDbContext.SaveChanges();
+            return this.RedirectToAction(nameof(Index));
+        }
+    }
+}
