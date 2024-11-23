915e16305a56ecda1086293a4a5a2add6a2b995c
Hristina Zheleva <122983011+Hrisi8074@users.noreply.github.com>
2024-11-23 21:56:59 +0200

    Update StudentController.cs

--- AplicationByScool/Controllers/StudentController.cs
+++ AplicationByScool/Controllers/StudentController.cs
@@ -1,50 +1,209 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SchoolDairy.Data;
 using SchoolDairy.Data.Models;
+using SchoolDairy.Models.Student;
+
+
 
 namespace SchoolDairy.Controllers
 {
     public class StudentController : Controller
     {
-        private readonly SchoolDairyDbContext schoolDairyDbContext;
 
-        public StudentController(SchoolDairyDbContext schoolDairyDbContext)
+        private readonly SchoolDairyDbContext context;
+
+        public StudentController(SchoolDairyDbContext context)
         {
-            this.schoolDairyDbContext = schoolDairyDbContext;
+           this. context = context;
         }
 
-        [HttpGet]
+
         public IActionResult Index()
         {
-            IEnumerable<Student> students = this.schoolDairyDbContext
-                 .Students
-                 .ToList();
+            var students = context.Students
+                .Include(s => s.StudentSpeciality)
+                .Include(s => s.Parent)
+                .Include(s => s.Teacher)
+                .ToList(); 
 
-            return View(students);
+            return View(students); 
         }
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult AddStudent()
         {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Student student)
-        {
-            this.schoolDairyDbContext.Students.Add(student);
-            this.schoolDairyDbContext.SaveChanges();
-            return this.RedirectToAction(nameof(Index));
-        }
-        public IActionResult Details(int Id)
-        {
-            
+            var viewModel = new AddStudentViewModel
+            {
+                Specialties = context.StudentSpecialities.Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.Name
+                }),
+                Parents = context.Parents.Select(p => new SelectListItem
+                {
+                    Value = p.Id.ToString(),
+                    Text = p.FirstName
+                }),
+                Teachers = context.Teachers.Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.FirstName
+                })
+            };
 
-            Student student = schoolDairyDbContext.Students
-                .FirstOrDefault(p => p.Id == Id);
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddStudent(AddStudentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                model.Specialties = context.StudentSpecialities.Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.Name
+                });
+                model.Parents = context.Parents.Select(p => new SelectListItem
+                {
+                    Value = p.Id.ToString(),
+                    Text = p.FirstName,
+
+                });
+                model.Teachers = context.Teachers.Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.FirstName,
+                });
+
+                return View(model);
+            }
+
+            var student = new Student
+            {
+                FirstName = model.FirstName,
+                MiddleName = model.MiddleName,
+                LastName = model.LastName,
+                Classroom = model.Classroom,
+                Number = model.Number,
+                FormOfTraining = model.FormOfTraining,
+                StudentSpeciality = context.StudentSpecialities.Find(model.SpecialtyId),
+                ParentId = model.ParentId,
+                TeacherId = model.TeacherId
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditStudent(int id)
+        {
+          
+            var student = context.Students
+                .Include(s => s.StudentSpeciality)
+                .Include(s => s.Parent)
+                .Include(s => s.Teacher)
+                .FirstOrDefault(s => s.Id == id);
+
             if (student == null)
             {
-                return NotFound();
+                return NotFound(); 
             }
-            return View(student);
+
+           
+            var viewModel = new EditStudentViewModel
+            {
+                Id = student.Id,
+                FirstName = student.FirstName,
+                MiddleName = student.MiddleName,
+                LastName = student.LastName,
+                Classroom = student.Classroom,
+                Number = student.Number,
+                FormOfTraining = student.FormOfTraining,
+                SpecialtyId = student.StudentSpeciality?.Id,
+                ParentId = student.ParentId,
+                TeacherId = student.TeacherId,
+                Specialties = context.StudentSpecialities.Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.Name
+                }).ToList(),
+                Parents = context.Parents.Select(p => new SelectListItem
+                {
+                    Value = p.Id.ToString(),
+                    Text = p.FirstName
+                }).ToList(),
+                Teachers = context.Teachers.Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.FirstName
+                }).ToList()
+            };
+
+            return View(viewModel); 
         }
+        [HttpPost]
+        public IActionResult EditStudent(EditStudentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+               
+                model.Specialties = context.StudentSpecialities.Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.Name
+                }).ToList();
+                model.Parents = context.Parents.Select(p => new SelectListItem
+                {
+                    Value = p.Id.ToString(),
+                    Text = p.FirstName
+                }).ToList();
+                model.Teachers = context.Teachers.Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.FirstName
+                }).ToList();
+
+                return View(model); 
+            }
+
+           
+            var student = context.Students.FirstOrDefault(s => s.Id == model.Id);
+            if (student == null)
+            {
+                return NotFound(); 
+            }
+
+            
+            student.FirstName = model.FirstName;
+            student.MiddleName = model.MiddleName;
+            student.LastName = model.LastName;
+            student.Classroom = model.Classroom;
+            student.Number = model.Number;
+            student.FormOfTraining = model.FormOfTraining;
+            student.StudentSpeciality = context.StudentSpecialities.Find(model.SpecialtyId); 
+            student.ParentId = (int)model.ParentId; 
+            student.TeacherId = (int) model.TeacherId;
+
+            
+            context.SaveChanges();
+
+           
+            return RedirectToAction("Index");
+        }
+       
+
     }
+
 }
+
+        
+
+ 
+
