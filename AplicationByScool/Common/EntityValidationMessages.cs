339673f7a1ed6722e4754d117fbd335f5927af50
Hristina Zheleva <122983011+Hrisi8074@users.noreply.github.com>
2024-11-23 21:57:37 +0200

    Update EntityValidationMessages.cs

--- AplicationByScool/Common/EntityValidationMessages.cs
+++ AplicationByScool/Common/EntityValidationMessages.cs
@@ -1,8 +1,11 @@
-﻿namespace SchoolDairy.Common
+namespace SchoolDairy.Common
 {
     public static class EntityValidationMessages
     {
         public const string FirstNameMessages = "No field filled in for First name";
+
+        public const string MiddleNameMessages = "No field filled in for Middle name";
+
         public const string LastNameMessages = "No field filled in for Last name";
         public const string PhoneNumberMessages = "Invalid phone number";
         public const string EmailAddressMessages = "The address already exists";
